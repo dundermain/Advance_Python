b1a5363956eac1494ec5a6b639bb3d1d60c92d69
dundermain <sachin68834@gmail.com>
2023-11-28 00:21:21 +0530

    added proxy design patterns

---
+++ proxy_design_pattern.py
@@ -0,0 +1,25 @@
+# proxy design pattern are kind of decorator where object is created therough a proxy class which inturn is connected to main class
+
+#lets see an example
+
+from abc import ABC, abstractmethod
+
+class Polygon(ABC):
+    @abstractmethod
+    def noofsides(self):
+        pass
+
+class Triangle(Polygon):
+    def noofsides(self):
+        print("I have 3 sides")
+
+class ProxyPolygon(Polygon):             #here we created a proxy class that is a subclass of main polygon class
+
+    def __init__(self):
+        self.triangle = Triangle()      #now here instead of instantiating directly, we instantiated the triangle class through proxy class
+
+    def noofsides(self):
+        return self.triangle.noofsides()
+    
+
+ProxyPolygon().noofsides()
